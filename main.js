9b66fbedec9eb11c47811748bbfe70b2a341ab64
Mark Arts <markarts@upcmail.nl>
2012-08-26 05:37:55 -0400

    xml load working+first xml test preset

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('init');
+$(document).ready(init);
+
+var width;
+var height;
+
+function init()
+{
+    console.log('init');
+    
+    width = $('body').width();
+    height = $('body').height();
+    console.log(width);
+    console.log(height);
+    
+    getXML();
+}
+
+function getXML()
+{
+    $.ajax({
+        url: "presets.xml",
+        success: parseXML,
+        dataType: "xml"
+    });    
+}
+function parseXML(xml)
+{
+    console.log(xml);  
+}
