52c2f9bc8431dfd2890bc9d75e44ca06455adc2e
Mark Arts <markarts@upcmail.nl>
2012-08-31 16:07:52 -0400

    divs worden nu met x/y cordinaten ut een preset object geladen en als object neergezet

--- main.js
+++ main.js
@@ -12,9 +12,35 @@ function init()
     console.log(width);
     console.log(height);
     
-    getXML();
+    //getXML();
+    getPreset();
+}
+var div = function(type)
+{
+    console.log(type);
+    var cord = preset1[type].split(',');
+    this.x = cord[0];
+    this.y = cord[1];
+    $('body').append('<div class="test" id="'+type+'">');
+    this.dom = $('body').children('#'+type);
+    $(this.dom).css('top',this.y)
+    $(this.dom).css('left',this.x)
 }
 
+
+var divs = {};
+
+function getPreset()
+{
+    console.log(preset1);   
+    for(var i in preset1)
+    {
+        divs[i] = new div(i);
+    }
+    
+}
+
+/*  begin van het probeeren van het ophalen van de preset data met xml. maar met objects leek em makelijker
 function getXML()
 {
     $.ajax({
@@ -26,4 +52,4 @@ function getXML()
 function parseXML(xml)
 {
     console.log(xml);  
-}
+} */
---
+++ preset.js
@@ -0,0 +1,5 @@
+var preset1 = {
+    'menu':'30,60',
+    'submenu':'200,300',
+    'test':'0,0',
+}
