6bd94d03c685101ccc3758e2bb1d843d7ba115ca
Mark Arts <markarts@upcmail.nl>
2012-08-31 17:58:47 -0400

    verslepen werkt maar zonder rekening te houden met de begin positie van de muis

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).ready(init);
 
 var width;
 var height;
-
 function init()
 {
     console.log('init');
@@ -14,28 +13,56 @@ function init()
     
     //getXML();
     getPreset();
+    
+    
+    $('.test').mousedown(startMove);
+}
+function startMove(event)
+{
+    var type = $(this).attr('id');
+    target = divs[type];
+    
+    $('body').mouseup(function(event){
+       $('body').unbind('mousemove'); 
+    });
+    
+    $('body').bind('mousemove',function(event){        
+        target.x = event.pageX;
+        target.y = event.pageY;
+        
+        target.update();
+    });
 }
 var div = function(type)
 {
     console.log(type);
     var cord = preset1[type].split(',');
+    
     this.x = cord[0];
     this.y = cord[1];
+    
+    this.canMove = false;
+    
+    
     $('body').append('<div class="test" id="'+type+'">');
     this.dom = $('body').children('#'+type);
     $(this.dom).css('top',this.y)
     $(this.dom).css('left',this.x)
 }
-
+div.prototype.update = function()
+{
+    $(this.dom).css('top',this.y)
+    $(this.dom).css('left',this.x)    
+}
 
 var divs = {};
 
 function getPreset()
 {
     console.log(preset1);   
-    for(var i in preset1)
+    for(var type in preset1)
     {
-        divs[i] = new div(i);
+        divs[type] = new div(type);
     }
     
 }
